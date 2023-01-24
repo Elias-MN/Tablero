841327764197ec58c878173488a8d42045c4b0a6
Elías <elias010493@gmail.com>
2023-01-24 11:18:49 +0100

    Added text function

--- script.js
+++ script.js
@@ -57,18 +57,29 @@ function drawLine() {
     context.stroke();
 }
 
+function drawText() {
+    context.font = "15px Arial";
+    let arrayOfLines = inputText.value.split("\n");
+    let distanceLines = 0;
+    arrayOfLines.forEach(linea => {
+        context.fillText(linea, initialPointX, initialPointY + distanceLines);
+        distanceLines = distanceLines + 20;
+    });
+}
+
 let lineCounter = 1;
 let painting = false;
 let erasering = false;
+let writting = false;
 let limitsCanvas = canvas.getBoundingClientRect();
+const inputText = document.getElementById("inputText");
 function clickCanvas(event) {
-    if (erasering == true && painting == false) {
+    if (painting == false && erasering == true && writting == false) {
         initialPointX = event.clientX - limitsCanvas.left;
         initialPointY = event.clientY - limitsCanvas.top;
         eraserPositionCanvas();
     }
-
-    if (painting == true && erasering == false) {
+    if (painting == true && erasering == false && writting == false) {
         if (lineCounter == 1) {
             initialPointX = event.clientX - limitsCanvas.left;
             initialPointY = event.clientY - limitsCanvas.top;
@@ -79,6 +90,11 @@ function clickCanvas(event) {
             lineCounter = 1;
             drawLine();
         }
+    }
+    if (painting == false && erasering == false && writting == true) {
+        initialPointX = event.clientX - limitsCanvas.left;
+        initialPointY = event.clientY - limitsCanvas.top;
+        drawText();
     }
 }
 
@@ -94,25 +110,58 @@ function clearCanvas() {
 const eraserElement = document.getElementById("eraser");
 const eraserButton = document.getElementById("eraserButton");
 const lineButton = document.getElementById("lineButton");
+const textButton = document.getElementById("textButton");
 function activateEraser() {
     erasering == true ? erasering = false : erasering = true;
     if (painting) {
-        painting = false;
-        lineButton.classList.remove("activateButton");
+        desactivatePainting();
+    }
+    if (writting) {
+        desactivateWritting();
     }
     eraserButton.classList.contains("activateButton") ? eraserButton.classList.remove("activateButton") : eraserButton.classList.add("activateButton");
     eraserElement.style.display == "block" ? eraserElement.style.display = "none" : eraserElement.style.display = "block";
 }
 
-function activateLine() {
+function activatePainting() {
     painting == true ? painting = false : painting = true;
     if (erasering) {
-        erasering = false;
-        eraserButton.classList.remove("activateButton");
-        eraserElement.style.display = "none";
+        desactivateEraser();
+    }
+    if (writting) {
+        desactivateWritting();
     }
     lineButton.classList.contains("activateButton") ? lineButton.classList.remove("activateButton") : lineButton.classList.add("activateButton");
 }
+
+function activateText() {
+    writting == true ? writting = false : writting = true;
+    if (painting) {
+        desactivatePainting();
+    }
+    if (erasering) {
+        desactivateEraser();
+    }
+    textButton.classList.contains("activateButton") ? textButton.classList.remove("activateButton") : textButton.classList.add("activateButton");
+
+}
+
+function desactivateEraser() {
+    erasering = false;
+    eraserButton.classList.remove("activateButton");
+    eraserElement.style.display = "none";
+}
+
+function desactivatePainting() {
+    painting = false;
+    lineButton.classList.remove("activateButton");
+}
+
+function desactivateWritting() {
+    writting = false;
+    textButton.classList.remove("activateButton");
+}
+
 
 let eraserWidth = 100;
 let eraserHeight = 100;
@@ -131,3 +180,4 @@ canvas.addEventListener("mousemove", function (event) {
         eraserElement.style.top = y + "px";
     }
 });
+
